495ab468beb8ffb2a294c2ee5e9138287c7bbdd4
moebiusmania <moebiusmania@gmail.com>
2024-11-12 14:28:31 +0100

    chore: vite & CI conf aligned to other projects

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "vite";
 
+const id: string = "/mohole-2012/";
+
+const isPROD: boolean = process.env.NODE_ENV === "production";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3000,
   },
-  base: "/mohole-2012/",
+  base: isPROD ? id : "/",
 });
